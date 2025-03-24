7ae14726fb41fa67965a17e4d60016aeeed62521
Vaibhavkumar26 <vaibhavkumar4488@gmail.com>
2025-03-24 06:47:42 +0530

    fiffth

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
 
